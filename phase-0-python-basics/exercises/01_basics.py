753890170a9387a0cf370cb9a1cf7909402d0aa6
Phan Thanh Đạt <datp3877@gmail.com>
2026-04-24 22:13:19 +0700

    Update 01_basics.py

--- phase-0-python-basics/exercises/01_basics.py
+++ phase-0-python-basics/exercises/01_basics.py
@@ -11,7 +11,6 @@ Chạy: python 01_basics.py
 # In ra: Hello, FastAPI!
 # TODO: viết code dưới đây
 
-print("Hello, FastApi!")
 
 # --- Bài 2: Biến và kiểu ---
 # Khai báo các biến sau và in ra type của từng biến:
@@ -22,23 +21,10 @@ print("Hello, FastApi!")
 # Gợi ý: dùng type(...)
 # TODO:
 
-age = 25
-height = 1.75
-name = "An"
-is_student = True
-
-print(type(age))
-print(type(height))
-print(type(name))
-print(type(is_student))
-
 # --- Bài 3: Input ---
 # Hỏi user "Tên bạn là gì? ", lấy input, in ra "Xin chào <tên>"
 # TODO:
 
-name = input("Tên bạn là gì?")
-
-print(f"Xin chào {name}")
 
 # --- Bài 4: Arithmetic ---
 # Viết chương trình tính BMI
@@ -47,13 +33,6 @@ print(f"Xin chào {name}")
 # - In ra BMI với 2 chữ số thập phân
 # Gợi ý: float(input(...)), f"{bmi:.2f}"
 # TODO:
-
-weight = float(input("Cân nặng của bạn là:"))
-height = float(input("Chiều cao của bạn là:"))
-
-bmi = weight / (height**2)
-
-print(f"BMI của bạn là:{bmi:.2f}")
 
 # --- Bài 5: String formatting ---
 # Có 3 biến: name, age, city
@@ -64,15 +43,6 @@ print(f"BMI của bạn là:{bmi:.2f}")
 # Output: "Tôi là An, 25 tuổi, sống ở Hà Nội."
 # TODO:
 
-name = "An"
-age = 25
-city = "Hà Nội"
-
-print("Tôi là " + name + ", " + str(age) + " tuổi, sống ở " + city + ".")
-
-print("Tôi là {}, {} tuổi, sống ở {}.".format(name, age, city))
-
-print(f"Tôi là {name}, {age} tuổi, sống ở {city}.")
 
 # --- Bài 6: Điều kiện ---
 # Phân loại BMI:
@@ -83,19 +53,6 @@ print(f"Tôi là {name}, {age} tuổi, sống ở {city}.")
 # Lấy BMI từ bài 4, in ra phân loại
 # TODO:
 
-weight = float(input("Cân nặng của bạn là:"))
-height = float(input("Chiều cao của bạn là:"))
-
-bmi = weight / (height**2)
-
-if bmi < 18.5:
-    print("Gầy")
-elif bmi < 25:
-    print("Bình thường")
-elif bmi < 30:
-    print("Thừa cân")
-else:
-    print("Béo phì")
 
 # --- Bài 7: Bonus ---
 # Viết chương trình đoán số:
@@ -105,19 +62,4 @@ else:
 # Gợi ý: import random, while True, break
 # TODO:
 
-import random
 
-number = random.randint(1, 100)
-number_of_guess = 0
-
-while True:
-    guess = int(input("Nhập số bạn đoán:"))
-    number_of_guess += 1
-
-    if guess == number:
-        print(f"Bạn đã đoán đúng sau {number_of_guess} lần đoán")
-        break
-    elif guess < number:
-        print("Lớn hơn")
-    else:
-        print("Nhỏ hơn")
